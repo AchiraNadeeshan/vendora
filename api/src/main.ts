06184fd77473a1b20e027d658c14bb9e29bbd067
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2026-02-20 18:13:13 +0530

    chore: setup nestjs pips

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Set global prefix for all routes
   app.setGlobalPrefix('api/v1');
+
+  // Set global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
