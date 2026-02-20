4539faa00ae19b7bf1852d18ce4e6361cbf8166d
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2026-02-20 17:54:32 +0530

    feat: add login and logout functionality

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,19 +1,47 @@
-import { Body, Controller, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
 import { RefreshTokenGuard } from './guards/refresh-token.guard';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   // Register api
+  @Post('register')
+  @HttpCode(201)
   async register(@Body() RegisterDto: RegisterDto): Promise<AuthResponseDto> {
     return await this.authService.register(RegisterDto);
   }
   // refresh access token
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(RefreshTokenGuard)
   async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
     return await this.authService.refreshTokens(userId);
+  }
+  // logout user and invalidate refresh token
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
+    await this.authService.logout(userId);
+    return { message: 'Successfully logged out' };
+  }
+  // Login method to authenticate a user
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
+    return await this.authService.login(loginDto);
   }
 }
--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -11,6 +11,7 @@ import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { randomBytes } from 'crypto';
 import { ConfigService } from '@nestjs/config';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -127,6 +128,41 @@ export class AuthService {
     return {
       ...tokens,
       user,
+    };
+  }
+
+  // Log out
+  async logout(userId: string): Promise<void> {
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { refreshToken: null },
+    });
+  }
+
+  // Login method to authenticate a user
+  async login(loginDto: LoginDto): Promise<AuthResponseDto> {
+    const { email, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const tokens = await this.generateTokens(user.id, user.email);
+    await this.updateRefreshToken(user.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: {
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+      },
     };
   }
 }
