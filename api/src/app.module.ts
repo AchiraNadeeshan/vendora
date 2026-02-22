92a7b913514b7ff85195e1a389f589fb9b62b99a
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2026-02-22 14:29:49 +0530

    chore: generate module controller and service classes for payments module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -9,6 +9,7 @@ import { CategoryModule } from './modules/category/category.module';
 import { ProductsModule } from './modules/products/products.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
     CategoryModule,
     ProductsModule,
     OrdersModule,
+    PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
